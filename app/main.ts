335fe073414950be6eff8c87ffe08ecc4fdc5c69
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2025-05-04 16:07:08 +0200

    Attempt #6

--- app/main.ts
+++ app/main.ts
@@ -6,6 +6,12 @@ const rl = createInterface({
 })
 
 let closed = false
+const commands = [
+  'echo',
+  'exit',
+  'type'
+]
+
 function nextQuestion() {
   rl.question('$ ', answer => {
     const literals = answer.split(' ')
@@ -20,6 +26,13 @@ function nextQuestion() {
       }
     } else if (command === 'echo') {
       console.log(`${literals.slice(1).join(' ')}`)
+    } else if (command === 'type') {
+      const typeCommand = literals[1]
+      if (commands.includes(typeCommand)) {
+        console.log(`${typeCommand} is a shell builtin`)
+      } else {
+        console.log(`${typeCommand}: command not found`)
+      }
     } else {
       console.log(`${command}: command not found`)
     }
