837231f0d128ef65936467e755e95cf21a28f404
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2025-05-04 15:13:18 +0200

    Attempt #1

--- app/main.ts
+++ app/main.ts
@@ -5,27 +5,11 @@ const rl = createInterface({
   output: process.stdout,
 });
 
-// Uncomment this block to pass the first stage
-rl.question("$ ", (answer) => {
-  console.log(`${answer}: command not found`);
-  // if (answer === "ls") {
-  //   console.log("app");
-  // } else if (answer === "cd app") {
-  //   console.log("app");
-  // } else if (answer === "cd ..") {
-  //   console.log("home");
-  // } else if (answer === "cd home") {
-  //   console.log("home");
-  // } else if (answer === "cd /") {
-  //   console.log("/");
-  // } else if (answer === "cd /home") {
-  //   console.log("/home");
-  // } else if (answer === "cd /app") {
-  //   console.log("/app");
-  // } else if (answer === "cd /home/user") {
-  //   console.log("/home/user");
-  // } else {
-  //   console.log("Command not found");
-  // }
-  rl.close();
-});
+function nextQuestion() {
+  rl.question("$ ", (answer) => {
+    console.log(`${answer}: command not found`);
+    nextQuestion()
+  })
+}
+
+nextQuestion()
